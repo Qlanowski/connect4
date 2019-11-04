fc7d9460266124c8de33a3ad346a7a7bc1152f68
Karol Ulanowski <ulanowskikarol@gmail.com>
2019-11-04 15:11:37 +0100

    add parameters to start game

--- console/src/app.ts
+++ console/src/app.ts
@@ -2,6 +2,33 @@ import { Bot } from "./shared/bot";
 import { McstBot } from "./mcst/mcst";
 import { Game } from "./shared/game";
 import * as ReadLine from "readline-sync"
+import minimist from "minimist";
+
+type ArgumentsShape = {
+    columns: string;
+    rows: string;
+    forWin: string;
+    botId: string;
+    turn: string;
+    timeout: string;
+};
+const args = minimist<ArgumentsShape>(process.argv.slice(2), {
+    boolean: ["help"],
+    string: ["columns", "rows", "forWin", "botId", "turn", "timeout"]
+});
+
+function printHelp(): void {
+    console.log("Connect4 console game usage: ");
+    console.log("");
+    console.log("--help        print help");
+    console.log("--columns    columns on board");
+    console.log("--rows       rows on board");
+    console.log("--forWin     count of checkers in row for win");
+    console.log("--botId      selecting bot to play with");
+    console.log("--turn       who starts 0-human 1-bot");
+    console.log("--timeout    time for bot to think in milliseconds");
+    console.log("");
+}
 
 function readNumber(text: string, allowedvalues: number[]): number {
     while (true) {
@@ -18,20 +45,7 @@ function readNumber(text: string, allowedvalues: number[]): number {
     }
 }
 
-function run() {
-    // let botId: number = readNumber("BotId 0,1,2: ", [0, 1, 2]);
-    // let turn: number = readNumber("Who starts 0-human, 1-bot: ", [0, 1]);
-    // let columns: number = readNumber("How many columns: ", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    // let rows: number = readNumber("How many rows: ", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    // let inRowForWin: number = readNumber("How many in row to win: ", [2, 3, 4, 5, 6]);
-    // let timeout: number = readNumber("Time in ms for bot to think: ", []);
-    let botId: number = 0;
-    let turn: number = 0;
-    let columns: number = 5;
-    let rows: number = 5;
-    let inRowForWin: number = 3;
-    let timeout: number = 1000;
-
+function run(columns: number, rows: number, inRowForWin: number, botId: number, turn: number, timeout: number) {
     let bot: Bot;
 
     switch (botId) {
@@ -45,7 +59,6 @@ function run() {
     let game: Game = new Game(columns, rows, inRowForWin);
 
     while (game.gameOn()) {
-        game.printBoard();
         if (turn === 0) {
             let move = readNumber("Select column:", game.allowedMoves());
             bot.playerMove(move);
@@ -65,10 +78,24 @@ function run() {
             move = bot.makeMove();
             game.move(move, turn);
             turn = 0;
+            game.printBoard();
         }
 
     }
-    game.printBoard();
     game.printWhoWon();
 }
-run();
+function readArgs() {
+    if (args.columns && args.rows && args.forWin && args.botId && args.turn && args.timeout) {
+        let columns: number = +args.columns;
+        let rows: number = +args.rows;
+        let inRowForWin: number = +args.forWin;
+        let botId: number = +args.botId;
+        let turn: number = +args.turn;
+        let timeout: number = +args.timeout;
+        run(columns, rows, inRowForWin, botId, turn, timeout);
+    } else {
+        printHelp();
+    }
+}
+
+readArgs();
