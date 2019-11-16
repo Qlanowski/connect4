95b8451400b98b3aff4aa695bde945ed2c254876
Jakub Waszkiewicz <qbazilla@hotmail.com>
2019-11-16 15:25:28 +0100

    minmax bot implemented, no specific heuristic yet

--- console/src/minmax/minMax.ts
+++ console/src/minmax/minMax.ts
@@ -1,11 +1,36 @@
 import { Bot } from "../shared/bot";
+import { Board } from "../shared/board";
+import { Player } from "../shared/player";
+import { MinMaxHeuristic } from "./minMaxHeuristic";
 
 export class MinMaxBot implements Bot {
-    playerMove(col: number): void {
-        throw new Error("Method not implemented.");
+    // Bot is Player 0 - no idea why I have to assume that
+    private currentBoard: Board;
+    private heuristic: MinMaxHeuristic;
+
+    constructor(columns: number, rows: number, inRow: number) {
+        let boardArr: Player[][] = new Array(columns).fill(0).map(() => Array(rows).fill(Player.None));
+        let heights: number[] = new Array(columns).fill(0);
+        this.currentBoard = new Board(columns, rows, inRow, boardArr, heights);
     }
-    
+
+    playerMove(move: number): void {
+        this.currentBoard.move(move, Player.Player1);
+    }
+
     makeMove(): number {
-        throw new Error("Method not implemented.");
+        let bestScore = -1;
+        let bestMove = -1;
+        let allowedMoves = this.currentBoard.allowedMoves();
+        allowedMoves.forEach(move => {
+            this.currentBoard.move(move, Player.Player0);
+            let score = this.heuristic.getScore(this.currentBoard);
+            this.currentBoard.undoMove(move);
+            if(bestScore < score) {
+                bestScore = score;
+                bestMove = move;
+            }
+        });
+        return bestMove;
     }
 }
