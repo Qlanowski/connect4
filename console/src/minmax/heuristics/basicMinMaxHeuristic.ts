7257b97d596249d89771aa2a95c75ce1e4e7f998
Jakub Waszkiewicz <qbazilla@hotmail.com>
2019-11-16 16:43:59 +0100

    methods operating on board extracted to helper class

--- console/src/minmax/heuristics/basicMinMaxHeuristic.ts
+++ console/src/minmax/heuristics/basicMinMaxHeuristic.ts
@@ -2,12 +2,13 @@ import { MinMaxHeuristic } from "./minMaxHeuristic";
 import { Board } from "../../shared/board";
 import { Player } from "../../shared/player";
 import { Result } from "../../shared/result";
+import { BoardHelper } from "../boardHelper";
 
 export class BasicMinMaxHeuristic implements MinMaxHeuristic {
     getScore(board: Board, player: Player): number {
         let allowedMoves = board.allowedMoves();
         allowedMoves.forEach(move => {
-            if (this.isWinningMove(board, move, player)) {
+            if (BoardHelper.isWinningMove(board, move, player)) {
                 return (board.columns * board.rows + 1 - board.moveCounter) / 2;
             }
         });
@@ -16,7 +17,7 @@ export class BasicMinMaxHeuristic implements MinMaxHeuristic {
         allowedMoves.forEach(move => {
             let clonedBoard = board.clone();
             clonedBoard.move(move, player);
-            let actualScore = this.getScore(clonedBoard, this.getOpponent(player));
+            let actualScore = this.getScore(clonedBoard, BoardHelper.getOpponent(player));
             if (actualScore > actualBestScore) {
                 actualBestScore = actualScore
             }
@@ -24,28 +25,4 @@ export class BasicMinMaxHeuristic implements MinMaxHeuristic {
 
         return actualBestScore;
     }
-
-    private isWinningMove(board: Board, move: number, player: Player) {
-        let clonedBoard = board.clone();
-        clonedBoard.move(move, player);
-        let result = clonedBoard.getResult();
-        return this.isWinner(player, result);
-    }
-
-    private isWinner(player: Player, result: Result) {
-        return (player === Player.Player0 && result === Result.WonPlayer0)
-        || (player === Player.Player1 && result === Result.WonPlayer1);
-    }
-
-    private getOpponent(myPlayer: Player): Player {
-        switch(myPlayer) {
-            case Player.Player0:
-                return Player.Player1;
-            case Player.Player1:
-                return Player.Player0;
-            default:
-                return Player.None;
-        }
-    }
-
 }
