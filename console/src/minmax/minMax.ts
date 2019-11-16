cf69dafcfe0408805f6d7ef7f163084824f6d995
Jakub Waszkiewicz <qbazilla@hotmail.com>
2019-11-16 15:34:57 +0100

    added timeout to minmaxbot constructor

--- console/src/minmax/minMax.ts
+++ console/src/minmax/minMax.ts
@@ -5,10 +5,12 @@ import { MinMaxHeuristic } from "./minMaxHeuristic";
 
 export class MinMaxBot implements Bot {
     // Bot is Player 0 - no idea why I have to assume that
+    private timeout: number
     private currentBoard: Board;
     private heuristic: MinMaxHeuristic;
 
-    constructor(columns: number, rows: number, inRow: number) {
+    constructor(columns: number, rows: number, inRow: number, timeout: number) {
+        this.timeout = timeout;
         let boardArr: Player[][] = new Array(columns).fill(0).map(() => Array(rows).fill(Player.None));
         let heights: number[] = new Array(columns).fill(0);
         this.currentBoard = new Board(columns, rows, inRow, boardArr, heights);
