fa2971432859d6813923c2a44819bf21c4d43e37
Jakub Waszkiewicz <qbazilla@hotmail.com>
2019-11-16 14:24:55 +0100

    interface for heuristic, not implemented min max bot

---
+++ console/src/minmax/minMax.ts
@@ -0,0 +1,11 @@
+import { Bot } from "../shared/bot";
+
+export class MinMaxBot implements Bot {
+    playerMove(col: number): void {
+        throw new Error("Method not implemented.");
+    }
+    
+    makeMove(): number {
+        throw new Error("Method not implemented.");
+    }
+}
---
+++ console/src/minmax/minMaxHeuristic.ts
@@ -0,0 +1,5 @@
+import { Board } from "../shared/board";
+
+export interface MinMaxHeuristic {
+    getScore(board: Board): number;
+}
