4dbd4dccf26ff8da591b14618ae4e64c01cac3dc
Jakub Waszkiewicz <qbazilla@hotmail.com>
2019-11-23 18:08:58 +0100

    implemented status updation and calculating a winner

--- console/src/minmax/board/minMaxBoard.ts
+++ console/src/minmax/board/minMaxBoard.ts
@@ -105,7 +105,125 @@ export class MinMaxBoard {
         }
     }
 
+    private getVerticalWinner(): Player {
+        for (let x = 0; x < this._width; x++) {
+            let inRow = 0;
+            let potentialWinner = this._fields[x][0];
+            for (let y = 0; y < this._piecesInColumnCount[x] && potentialWinner !== Player.None; y++) {
+                if (this._fields[x][y] === potentialWinner) {
+                    inRow++;
+                }
+                else {
+                    if (inRow >= this._inRowToWin) {
+                        return potentialWinner;
+                    }
+                    inRow = 1;
+                    potentialWinner = this._fields[x][y];
+                }
+            }
+            if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                return potentialWinner;
+            }
+        }
+
+        return Player.None;
+    }
+
+    private getHorizontalWinner(): Player {
+        for (let y = 0; y <= this._height; y++) {
+            let inRow = 0;
+            let potentialWinner = this._fields[0][y];
+            for (let x = 0; x < this._width; x++) {
+                if (this._fields[x][y] === potentialWinner) {
+                    inRow++;
+                }
+                else {
+                    if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                        return potentialWinner;
+                    }
+                    inRow = 1;
+                    potentialWinner = this._fields[x][y];
+                }
+            }
+            if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                return potentialWinner;
+            }
+        }
+
+        return Player.None;
+    }
+
+    private getDiagonalWinner(): Player {
+        for (let x = 0; x < this._width; x++) {
+            let inRow = 0;
+            let potentialWinner = this._fields[x][0];
+            for (let y = 0; y < Math.min(this._height, this._width - x); y++) {
+                if (this._fields[x + y][y] === potentialWinner) {
+                    inRow++;
+                }
+                else {
+                    if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                        return potentialWinner;
+                    }
+                    inRow = 1;
+                    potentialWinner = this._fields[x + y][y];
+                }
+            }
+            if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                return potentialWinner;
+            }
+        }
+
+        return Player.None;
+    }
+
+    private getAntiDiagonalWinner(): Player {
+        for (let x = 0; x < this._width; x++) {
+            let inRow = 0;
+            let potentialWinner = this._fields[x][0];
+            for (let y = 0; y < Math.min(this._height, x); y++) {
+                if (this._fields[x - y][y] === potentialWinner) {
+                    inRow++;
+                }
+                else {
+                    if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                        return potentialWinner;
+                    }
+                    inRow = 1;
+                    potentialWinner = this._fields[x - y][y];
+                }
+            }
+            if (inRow >= this._inRowToWin && potentialWinner !== Player.None) {
+                return potentialWinner;
+            }
+        }
+
+        return Player.None;
+    }
+
+    private getWinner(): Player {
+        const vertialWinner = this.getVerticalWinner();
+        if (vertialWinner !== Player.None) {
+            return vertialWinner;
+        }
+
+        const horizontalWinner = this.getHorizontalWinner();
+        if (horizontalWinner !== Player.None) {
+            return horizontalWinner;
+        }
+
+        const diagonalWinner = this.getDiagonalWinner();
+        if (diagonalWinner !== Player.None) {
+            return diagonalWinner;
+        }
+
+        return this.getAntiDiagonalWinner();
+    }
+
     private updateStatus() {
-        throw new Error('Not implemented');
+        const winner = this.getWinner();
+        if (winner !== Player.None) {
+            this.setWinner(winner);
+        }
     }
 }
