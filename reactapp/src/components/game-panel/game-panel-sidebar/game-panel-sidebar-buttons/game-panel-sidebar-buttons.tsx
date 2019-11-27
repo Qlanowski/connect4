73261f0e1b9fc81d2fad6d33dd82538dd0012e7d
Michał Urawski <michurawski@gmail.com>
2019-11-27 12:32:05 +0100

    priettier sidebar

--- reactapp/src/components/game-panel/game-panel-sidebar/game-panel-sidebar-buttons/game-panel-sidebar-buttons.tsx
+++ reactapp/src/components/game-panel/game-panel-sidebar/game-panel-sidebar-buttons/game-panel-sidebar-buttons.tsx
@@ -6,12 +6,31 @@ interface GamePanelSidebarButtonsProps {
     newGame: () => void;
 }
 
+export const DefaultButton = styled.button`
+    background: #fff;
+    margin: 5px auto;
+    border-radius: 6px;
+    height: 35px;
+    line-height: 35px;
+    border: 1px solid #aaa;
+    box-shadow: 0px;
+    outline: none;
+    transition: 0.15s;
+    padding: 0 10px;
+    margin: 0 4px;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+        background: #eee;
+    }
+  `
+
 export const GamePanelSidebarButtons: React.FC<GamePanelSidebarButtonsProps> = 
 ({gameRestarted, newGame}) => {
     return (
     <div>
-        <button onClick={() => gameRestarted()}>Restart game</button> 
-        <button onClick={() => newGame()}>New Game</button>
+        <DefaultButton onClick={() => gameRestarted()}>Restart game</DefaultButton> 
+        <DefaultButton onClick={() => newGame()}>New Game</DefaultButton>
     </div>
     )
 }
